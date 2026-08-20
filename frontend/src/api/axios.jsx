91f2cfb0988283e47327a27176c7b4efc580d9d4
alisher20020324-collab <alisher20020324@gmail.com>
2026-08-20 14:58:21 +0500

    change url

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstanse = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://onlainmagazin.onrender.com/api/v1",
   headers: { "Content-Type": "Application/json" },
 });
